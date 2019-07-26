58d5916b5ce54711f03e909449171d507a0aec2c
harisfeb19 <harisfeb19.gmail.com>
2019-07-26 12:00:08 +0500

    user table added

--- myqueries.sql
+++ myqueries.sql
@@ -15,3 +15,18 @@ insert into world.User values(1,'haris','123');
 select * from world.user;
 
 
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(225) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+insert into pos.user values (11,1,"haris@gmail.com","Haris","Ali","12345");
+update user set password = "$2a$10$EQ6Hd2xK2gVNLvU8EYahXu4OKRfsH1i9IO8ALmEqvyLD1jlQ1eiQ6";
+
+insert into pos.user values (2,1,"ali@gmail.com","Ali","Khan","$2a$10$JmsS6PfzeXKjw1G1w8QKpuruKsCgyGXj6qcLtjR2MrVM1og8BjAyS");
+delete from pos.user where id = 12;
+
+truncate pos.user;
